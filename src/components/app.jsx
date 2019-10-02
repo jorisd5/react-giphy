d1a0b069410a10dc2a2e15e296fd6eab7bfca154
Joris Dewaele <joris.dewaele@tutanota.com>
2019-10-02 22:18:46 +0200

    Start React Giphy

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene" />
+        <div className="right-scene" />
+      </div>
+    );
+  }
+}
+
+export default App;
