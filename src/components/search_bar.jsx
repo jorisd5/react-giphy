06eca21194ba695d1e63a09fa1e4bf328804b413
Joris Dewaele <joris.dewaele@tutanota.com>
2019-10-06 23:33:21 +0200

    Added value state to search_bar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+
   handleUpdate = (event) => {
-    debugger
-    this.props.searchFunction(event.target.value);
+    this.setState({ term: event.target.value });
+    const searchFunction = this.props;
+    searchFunction(event.target.value);
   }
 
 
   render() {
+    const { term } = this.state;
     return (
-      <input type="text" className="form-control form-search"
-        onChange={this.handleUpdate} />
+      <input
+        value={term}
+        type="text"
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+      />
     );
   }
 }
